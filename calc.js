fcec58aac78409b41da7f346b0ad5b8270d397ce
Cole Mickelson <cmickelson94@gmail.com>
2021-10-12 21:41:37 -0500

    basic functionality

--- calc.js
+++ calc.js
@@ -13,17 +13,14 @@ function multiply(x,y){
 function divide(x,y){
     return x/y;
 };
-
-function operate(num1,operator,num2){
-    return operator(num1,num2);
+function operate(operator,num1,num2){
+return operator(num1,num2);
 };
 
 
 function updateDisplay(value){
     const display=document.getElementById('display');
     display.innerText+=value;
-    console.log(value);
-    return value;
 
 }; 
 function clear(){
@@ -32,5 +29,28 @@ function clear(){
 
 function equals(){
     const display=document.getElementById('display');
-    display.innerText=operate()
-}
+    array=display.innerText.split(/(?=[\+\-\*\/])|(?<=[\+\-\*\/])/g)
+    if (array[1]==='+'){
+        op=add;
+    }
+    
+    
+    else if(array[1]==='-'){
+        op=subtract;
+    }
+    else if(array[1]==='*'){
+        op=multiply;
+    }
+    else if (array[1]==='/'){
+        op=divide;
+    }
+    else{alert(array)}
+    num1=parseFloat(array[0]);
+    num2=parseFloat(array[2]);
+    display.innerText=operate(op,num1,num2)
+    
+};
+const equalsBtn=document.getElementById('equalsBtn');
+equalsBtn.addEventListener('click',function(){equals()
+    
+});
