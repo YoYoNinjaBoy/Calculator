af7de9ef0b75728a7e25d2080f7a56b0809d3174
Cole Mickelson <cmickelson94@gmail.com>
2021-10-14 16:05:05 -0500

    backup before I gut everything and start over

--- calc.js
+++ calc.js
@@ -1,4 +1,5 @@
 const display=document.getElementById('display');
+const display2=document.getElementById('display2')
 function add(x,y){
     return x+y;
 };
@@ -20,90 +21,127 @@ return operator(num1,num2);
 
 
 function updateDisplay(value){
-    const display=document.getElementById('display');
     display.innerText+=value;
 
 }; 
 function clear(){
-
+    display.innerText='';
 };
+function trueClear(){
+    display.innerText='';
+    display2.innerText='';
+}
+function migrateDisplay(operator){
+display2.textContent+=display.textContent+operator;
+}
 
-function equals(){
-    array=display.innerText.split(/(?=[\+\-\*\/])|(?<=[\+\-\*\/])/g)
-    if (array[0==='-']){
-        array[0].concat(array[1])
-    }
-    if (array[1]==='+'){
-        operator=add;
-    }
-    
-    
-    else if(array[1]==='-'){
-        operator=subtract;
-    }
-    else if(array[1]==='*'){
-        operator=multiply;
-    }
-    else if (array[1]==='/'){
-        operator=divide;
-    }
-    else{alert('negatives bad me bad hit clear')}
-    num1=parseFloat(array[0]);
-    num2=parseFloat(array[2]);
-    display.innerText=operate(operator,num1,num2)
-    
-};
 const equalsBtn=document.getElementById('equalsBtn');
-equalsBtn.addEventListener('click',function(){equals()
-    });
+equalsBtn.addEventListener('click',function(){
+    equals();
+});
 const addBtn=document.getElementById('plusBtn');
 addBtn.addEventListener('click',function(){
-    if (
-        display.innerText.includes('\+')===true ||
-        display.innerText.includes('\-')===true ||
-        display.innerText.includes('\*')===true ||
-        display.innerText.includes('\/')===true){
-    equals()
+    if (isNaN(num1)===false && isNaN(num2)===true){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('+');
+        clear();
     }
-    updateDisplay("+")}
+    
+    else if((isNaN(num1)===false && isNaN(num2)===false)){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('+');
+        clear();
+    }
+    else{
+        num1=parseFloat(display.innerText);
+        migrateDisplay('+');
+        clear();
+    }
+    operator=add;
+}
 )
 
 const subBtn=document.getElementById('minusBtn');
 subBtn.addEventListener('click',function(){
-    if (
-        display.innerText.includes('\+')===true ||
-        display.innerText.includes('\-')===true ||
-        display.innerText.includes('\*')===true ||
-        display.innerText.includes('\/')===true){
-    equals()
+    if (isNaN(num1)===false && isNaN(num2)===true){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('-');
+        clear();
     }
-    updateDisplay("-")}
+    
+    else if((isNaN(num1)===false && isNaN(num2)===false)){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('-');
+        clear();
+    }
+    else{
+        num1=parseFloat(display.innerText);
+        migrateDisplay('-');
+        clear();
+    }
+    operator=subtract;
+}
 )
-
 const divBtn=document.getElementById('divideBtn');
 divBtn.addEventListener('click',function(){
-    if (
-        display.innerText.includes('\+')===true ||
-        display.innerText.includes('\-')===true ||
-        display.innerText.includes('\*')===true ||
-        display.innerText.includes('\/')===true){
-    equals()
+    if (isNaN(num1)===false && isNaN(num2)===true){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('/');
+        clear();
     }
-    updateDisplay("/")}
+    
+    else if((isNaN(num1)===false && isNaN(num2)===false)){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('/');
+        clear();
+    }
+    else{
+        num1=parseFloat(display.innerText);
+        migrateDisplay('/');
+        clear();
+    }
+    operator=divide;
+}
 )
 
 const mulBtn=document.getElementById('timesBtn');
 mulBtn.addEventListener('click',function(){
-    if (
-        display.innerText.includes('\+')===true ||
-        display.innerText.includes('\-')===true ||
-        display.innerText.includes('\*')===true ||
-        display.innerText.includes('\/')===true){
-    equals()
+    if (isNaN(num1)===false && isNaN(num2)===true){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('x');
+        clear();
     }
-    updateDisplay("*")}
+    
+    else if((isNaN(num1)===false && isNaN(num2)===false)){
+        equals();
+        num1=parseFloat(display.innerText);
+        migrateDisplay('x');
+        clear();
+    }
+    else{
+        num1=parseFloat(display.innerText);
+        migrateDisplay('x');
+        clear();
+    }
+    operator=multiply;
+}
 )
+
 const clrBtn=document.getElementById('clearBtn');
 clrBtn.addEventListener('click',function(){
-    display.innerText=''
+    trueClear();
 })
+function equals(){
+    num2=parseFloat(display.innerText);
+    migrateDisplay('=');
+    clear();
+    num1=operate(operator,num1,num2);
+    display.innerText=num1;
+};
