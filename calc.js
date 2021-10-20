e7ca41b65ca96d13dbe074852f8d557deeb6432c
Cole Mickelson <cmickelson94@gmail.com>
2021-10-19 21:49:59 -0500

    logic updated does not break anymore

--- calc.js
+++ calc.js
@@ -1,5 +1,11 @@
 const display=document.getElementById('display');
-const display2=document.getElementById('display2')
+const display2=document.getElementById('display2');
+let equalsPressed=false;
+let num1=undefined;
+let num2=undefined;
+display.innerText=0
+
+
 function add(x,y){
     return x+y;
 };
@@ -21,116 +27,146 @@ return operator(num1,num2);
 
 
 function updateDisplay(value){
+    if (display.innerText==='0'){
+        display.innerText=value;
+    }
+    else{
     display.innerText+=value;
-
+    }
 }; 
 function clear(){
     display.innerText='';
 };
 function trueClear(){
-    display.innerText='';
+    display.innerText='0';
     display2.innerText='';
+    num1=undefined;
+    num2=undefined;
+    operator=undefined;
 }
 function migrateDisplay(operator){
-display2.textContent+=display.textContent+operator;
+display2.textContent=display.textContent+operator;
 }
 
 const equalsBtn=document.getElementById('equalsBtn');
 equalsBtn.addEventListener('click',function(){
-    equals();
-});
-const addBtn=document.getElementById('plusBtn');
-addBtn.addEventListener('click',function(){
-    if (isNaN(num1)===false && isNaN(num2)===true){
-        equals();
-        num1=parseFloat(display.innerText);
-        migrateDisplay('+');
-        clear();
+    if(equalsPressed===true){
+
     }
-    
-    else if((isNaN(num1)===false && isNaN(num2)===false)){
-        equals();
-        num1=parseFloat(display.innerText);
-        migrateDisplay('+');
-        clear();
+    else if(display.innerText===''){
+        
     }
     else{
-        num1=parseFloat(display.innerText);
+    equals();
+    equalsPressed=true;
+    }
+});
+
+const addBtn=document.getElementById('plusBtn');
+addBtn.addEventListener('click',function(){
+    if (num1===undefined){
+        num1=parseFloat(display.innerText)
+        operator=add;
         migrateDisplay('+');
         clear();
     }
-    operator=add;
+    else if(equalsPressed===true){
+        equalsPressed=false;
+        operator=add;
+        migrateDisplay('+');
+        clear();
+    }
+    else if (isNaN(num1)===false && isNaN(num2)===true){
+        operator=add;
+        display2.innerText=display2.innerText.replace(/[-x/]/g,'+');
+    }
+    else{
+        equals();
+        migrateDisplay('+');
+        clear();
+        operator=add;
+    }
 }
 )
 
 const subBtn=document.getElementById('minusBtn');
 subBtn.addEventListener('click',function(){
-    if (isNaN(num1)===false && isNaN(num2)===true){
-        equals();
-        num1=parseFloat(display.innerText);
+    if (num1===undefined){
+        num1=parseFloat(display.innerText)
+        operator=subtract;
         migrateDisplay('-');
         clear();
     }
-    
-    else if((isNaN(num1)===false && isNaN(num2)===false)){
-        equals();
-        num1=parseFloat(display.innerText);
+    else if(equalsPressed===true){
+        equalsPressed=false;
+        operator=subtract;
         migrateDisplay('-');
         clear();
+    }
+    else if (isNaN(num1)===false && isNaN(num2)===true){
+        operator=subtract;
+        display2.innerText=display2.innerText.replace(/[+x/]/g,'-');
     }
     else{
-        num1=parseFloat(display.innerText);
+        equals();
         migrateDisplay('-');
         clear();
+        operator=subtract;
     }
-    operator=subtract;
 }
 )
+
 const divBtn=document.getElementById('divideBtn');
 divBtn.addEventListener('click',function(){
-    if (isNaN(num1)===false && isNaN(num2)===true){
-        equals();
-        num1=parseFloat(display.innerText);
+    if (num1===undefined){
+        num1=parseFloat(display.innerText)
+        operator=divide;
         migrateDisplay('/');
         clear();
     }
-    
-    else if((isNaN(num1)===false && isNaN(num2)===false)){
-        equals();
-        num1=parseFloat(display.innerText);
+    else if(equalsPressed===true){
+        equalsPressed=false;
+        operator=divide;
         migrateDisplay('/');
         clear();
+    }
+    else if (isNaN(num1)===false && isNaN(num2)===true){
+        operator=divide;
+        display2.innerText=display2.innerText.replace(/[+x-]/g,'/');
     }
     else{
-        num1=parseFloat(display.innerText);
+        equals();
         migrateDisplay('/');
         clear();
+        operator=divide;
     }
-    operator=divide;
 }
 )
 
 const mulBtn=document.getElementById('timesBtn');
 mulBtn.addEventListener('click',function(){
-    if (isNaN(num1)===false && isNaN(num2)===true){
-        equals();
-        num1=parseFloat(display.innerText);
+    if (num1===undefined){
+        num1=parseFloat(display.innerText)
+        operator=multiply;
         migrateDisplay('x');
         clear();
     }
-    
-    else if((isNaN(num1)===false && isNaN(num2)===false)){
-        equals();
-        num1=parseFloat(display.innerText);
+    else if(equalsPressed===true){
+        equalsPressed=false;
+        operator=multiply;
         migrateDisplay('x');
         clear();
+    }
+    else if (isNaN(num1)===false && isNaN(num2)===true){
+        operator=multiply;
+        display2.innerText=display2.innerText.replace(/[+/-]/g,'x');
     }
     else{
-        num1=parseFloat(display.innerText);
+        equals();
         migrateDisplay('x');
         clear();
+        operator=multiply;
     }
-    operator=multiply;
 }
 )
 
@@ -140,8 +176,8 @@ clrBtn.addEventListener('click',function(){
 })
 function equals(){
     num2=parseFloat(display.innerText);
-    migrateDisplay('=');
-    clear();
-    num1=operate(operator,num1,num2);
-    display.innerText=num1;
+    migrateDisplay('');
+    display.innerText=operate(operator,num1,num2);
+    num1=parseFloat(display.innerText);
+    num2=undefined;
 };
